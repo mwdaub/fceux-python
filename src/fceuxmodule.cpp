9065dc1b743657a39d045b1239e0cdcfc49419ad
Michael Daub <mwdaub@gmail.com>
2017-07-03 14:16:10 -0700

    Added support for movie recording; added more comments throughout the code.

--- src/fceuxmodule.cpp
+++ src/fceuxmodule.cpp
@@ -9,23 +9,45 @@ static PyObject *InvalidRomError;
 // "A" = 1, "B" = 2, "SELECT" = 4, "START" = 8, "UP" = 16, "DOWN" = 32, "LEFT" = 64, "RIGHT" = 128
 // Combinations of buttons are indicated by taking their sums.
 // To specify input for player 2, take the identical player 1 input and bit shift left by 8.
-unsigned int input;
+uint32 input;
 
+// Load a rom and, optionally, start recording an fceux movie. Since the emulation is
+// deterministic, the movie only needs to save the controller input at each frame. The movie can be
+// replayed in the fceux emulator by running:
+// fceux --playmov <movie_file_name> <rom_file_name>
+//
+// Arguments: romFileName, movieFileName
 static PyObject * fceux_load_rom(PyObject *self, PyObject *args) {
-  const char * arg;
+  char * romFileName;
+  char * movieFileName = "";
 
   // parse arguments
-  if (!PyArg_ParseTuple(args, "s", &arg)) {
+  if (!PyArg_ParseTuple(args, "s|s", &romFileName, &movieFileName)) {
     return NULL;
   }
 
-  if (!FCEUI_LoadGame(arg, 1)) {
+  if (!FCEUI_LoadGame(romFileName, 1)) {
     char *msg;
-    asprintf(&msg,"File %s is not a valid ROM or does not exist.", arg);
+    asprintf(&msg,"File %s is not a valid ROM or does not exist.", romFileName);
     PyErr_SetString(InvalidRomError, msg);
     return NULL;
   }
   FCEUI_SetInput(0, SI_GAMEPAD, &input, 0);
+  if (!(movieFileName[0] == '\0')) {
+    FCEUI_SaveMovie(movieFileName, MOVIE_FLAG_FROM_POWERON, L"");
+  }
+
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+// Close a rom and, if a movie is recording, stop recording the movie.
+static PyObject * fceux_close_rom(PyObject *self, PyObject *args) {
+  // parse arguments
+  if (!PyArg_ParseTuple(args, "")) {
+    return NULL;
+  }
+  FCEUI_CloseGame();
 
   Py_INCREF(Py_None);
   return Py_None;
@@ -34,21 +56,27 @@ static PyObject * fceux_load_rom(PyObject *self, PyObject *args) {
 // Dimensions of the game image.
 static long int dims[] = {240, 256, 3};
 
+// Emulate a single frame, returning the pixels of the game screen.
+// TODO: return sound data as well.
+//
+// Arguments: integer representing controller input for the frame. See comment above the input
+// variable for details.
+// Returns: NumPy array with shape dims and type uint8, containing the pixel data.
 static PyObject * fceux_emulate_frame(PyObject *self, PyObject *args) {
   // parse arguments
   if (!PyArg_ParseTuple(args, "I", &input)) {
     return NULL;
   }
 
-  unsigned char *gfx;
-  signed int *sound;
-  signed int ssize;
+  uint8 *gfx;
+  int32 *sound;
+  int32 ssize;
   FCEUI_Emulate(&gfx, &sound, &ssize, 0);
 
   PyObject *array = PyArray_SimpleNew(3, dims, NPY_UINT8);
-  unsigned char *data = (unsigned char *) PyArray_DATA(array);
+  uint8 *data = (unsigned char *) PyArray_DATA(array);
   for (size_t i = 0; i < 240*256; i++) {
-    unsigned char idx = gfx[i] - 128;
+    uint8 idx = gfx[i] - 128;
     data[3*i] = palo[idx].r;
     data[3*i+1] = palo[idx].g;
     data[3*i+2] = palo[idx].b;
@@ -56,26 +84,33 @@ static PyObject * fceux_emulate_frame(PyObject *self, PyObject *args) {
   return array;
 }
 
+// Read the value at the given memory address.
+//
+// Arguments: uint16 representing the memory address.
+// Returns: uint8 representing the memory value.
 static PyObject * fceux_read_memory(PyObject *self, PyObject *args) {
-  unsigned short arg;
+  uint16 memAddress;
 
   // parse arguments
-  if (!PyArg_ParseTuple(args, "H", &arg)) {
+  if (!PyArg_ParseTuple(args, "H", &memAddress)) {
     return NULL;
   }
 
   // read memory
-  unsigned char val = GetMem(arg);
-  return Py_BuildValue("I", val);
+  uint8 memValue = GetMem(memAddress);
+  return Py_BuildValue("I", memValue);
 }
 
+// List of fceux module methods.
 static PyMethodDef FceuxMethods[] = {
   { "load_rom", fceux_load_rom, METH_VARARGS, "Load NES rom" },
+  { "close_rom", fceux_close_rom, METH_VARARGS, "Close loaded NES rom" },
   { "emulate_frame", fceux_emulate_frame, METH_VARARGS, "Emulate a single frame" },
   { "read_memory", fceux_read_memory, METH_VARARGS, "Read the value at the given memory address" },
   { NULL, NULL, 0, NULL }
 };
 
+// Initialize the fceux module.
 PyMODINIT_FUNC initfceux(void)
 {
   PyObject *m;
--- src/fceuxmodule.h
+++ src/fceuxmodule.h
@@ -2,11 +2,14 @@
 #include <numpy/arrayobject.h>
 
 #include "fceux/git.h"
+#include "fceux/movie.h"
 #include "fceux/palette.h"
 
 // Forward declaration of required FCEUX functions.
 bool FCEUI_Initialize();
 void FCEUI_SetInput(int port, ESI type, void *ptr, int attrib);
 FCEUGI *FCEUI_LoadGame(const char *name, int OverwriteVidMode, bool silent = false);
-void FCEUI_Emulate(unsigned char **pXBuf, signed int **SoundBuf, signed int *SoundBufSize, int skip);
-unsigned char GetMem(unsigned short addr);
+void FCEUI_CloseGame();
+void FCEUI_SaveMovie(const char *filename, EMOVIE_FLAG flags, std::wstring author);
+void FCEUI_Emulate(uint8 **pXBuf, int32 **SoundBuf, int32 *SoundBufSize, int skip);
+uint8 GetMem(uint16 addr);
